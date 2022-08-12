8aa79c53b847320c85cdbd834c5246cbd9a18222
GaKon101 <tronglong28@gmail.com>
2022-08-12 11:52:40 +0700

    done Router

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -9,8 +9,8 @@ const HomeScreen = () => {
         <h1>
             Latest Products
             <Row>
-                {products.map(product => (
-                    <Col sm={12} md={6} lg={4} xl={3}>
+                {products.map((product) => (
+                    <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
                        <Product product={product} />
                     </Col>
                 ))}
