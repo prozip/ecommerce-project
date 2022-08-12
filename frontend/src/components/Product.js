83d41104ea97eca1bbc6fdad56f7a650c5f8c183
prozip <quachkimson2000@gmail.com>
2022-08-12 10:02:25 +0700

    fix rating

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,42 +1,41 @@
 import React from 'react'
 import { Card } from 'react-bootstrap'
-// import Rating from './Rating'
+import Rating from './Rating'
 
 const Product = ({ product }) => {
-  return (
-    <Card className='my-3 p-3 rounded'>
-        <a href={`/product/${product._id}`}>
-            <Card.Img src={product.image}variant='top' />
-        </a>
-
-        <Card.Body>
+    return (
+        <Card className='my-3 p-3 rounded'>
             <a href={`/product/${product._id}`}>
-                <Card.Title as='div'>
-                    <strong>
-                        {product.name}
-                    </strong>
-                </Card.Title>
-
-                <Card.Text as='div'>
-                    {/* <Rating value={product.rating} 
-                    text={`${product.numReviews} Reviews`} /> */}
-                </Card.Text>
+                <Card.Img src={product.image} variant='top' />
             </a>
 
-            <Card.Text as='div'>
-                {/* <div className='my-3'>
-                    {product.rating} from {product.numReviews} Reviews
+            <Card.Body>
+                <a href={`/product/${product._id}`}>
+                    <Card.Title as='div'>
+                        <strong>
+                            {product.name}
+                        </strong>
+                    </Card.Title>
 
-                </div> */}
-            </Card.Text>
+                    <Card.Text as='div'>
+                        <Rating value={product.rating}
+                            text={`${product.numReviews} Reviews`} />
+                    </Card.Text>
+                </a>
 
-            <Card.Text as='h3'> ${product.price}
-            </Card.Text>
+                <Card.Text as='div'>
+                    <div className='my-3'>
+                        {product.rating} from {product.numReviews} Reviews
+                    </div>
+                </Card.Text>
 
-        </Card.Body>
+                <Card.Text as='h3'> ${product.price}
+                </Card.Text>
 
-    </Card>
-  )
+            </Card.Body>
+
+        </Card>
+    )
 }
 
 export default Product
