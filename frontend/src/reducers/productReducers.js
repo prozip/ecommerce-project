8741d5e84cf035ff5e9067d2429d5810d99d2db9
GaKon101 <tronglong28@gmail.com>
2022-08-16 11:15:41 +0700

    done Redux

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,7 +1,10 @@
 import {
     PRODUCT_LIST_REQUEST,
     PRODUCT_LIST_SUCCESS,
-    PRODUCT_LIST_FAIL
+    PRODUCT_LIST_FAIL,
+    PRODUCT_DETAILS_REQUEST,
+    PRODUCT_DETAILS_SUCCESS,
+    PRODUCT_DETAILS_FAIL,
 }  from '../constants/productConstants'
 
 export const productListReducer =(state ={ products: [] }, action) => {
@@ -15,4 +18,18 @@ export const productListReducer =(state ={ products: [] }, action) => {
         default:
             return state
     }
+}
+
+export const productDetailsReducer =(state ={ product: { review: [] } },
+    action) => {
+        switch (action.type) {
+            case PRODUCT_DETAILS_REQUEST:
+                return { loading: true, ...state }
+            case PRODUCT_DETAILS_SUCCESS :
+                return { loading: false, product: action.payload}
+            case PRODUCT_DETAILS_FAIL:
+                return {loading: false, error: action.payload}
+            default:
+                return state
+        }
 }
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,28 +1,31 @@
-import React , {useState,useEffect} from 'react'
+import React , {useEffect} from 'react'
 import { Link, useParams } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
 import {Row, Col, Image, ListGroup, Card, Button, ListGroupItem} from 'react-bootstrap'
 import Rating from '../components/Rating'
-import axios from 'axios'
+import Message from '../components/Message'
+import Loader from '../components/Loader'
+import { listProductDetails } from '../actions/productActions'
 
 const ProductScreen = () => {
     const params = useParams()
     // const product = products.find((p)=>p._id === params.id)
 
-    const [product, setProduct] = useState({})
+    const dispatch = useDispatch()
+
+    const productDetails = useSelector(state => state.productDetails)
+    const { loading, error, product} = productDetails
       
     useEffect(() => {
-        const fetchProduct = async () => {
-            const {data} = await axios.get(`/api/products/${params.id}`)
-            setProduct(data)
-        }
-      
-        fetchProduct()
-     }, [params])
+        dispatch(listProductDetails(params.id))
+     }, [dispatch, params])
 
     return (<>
-        <Link className='btn btn-light my-3' to='/'> Go Back
+        <Link className='btn btn-light my-3' to='/'> 
+        Go Back
         </Link>
-        <Row>
+        {loading ? <Loader /> : error ? <Message variant = 'danger'>{error}</Message> : (
+            <Row>
             <Col md={6}>
                 <Image src={product.image} alt={product.name} fluid/>
             </Col>
@@ -75,6 +78,9 @@ const ProductScreen = () => {
                 </Card>
             </Col>
         </Row>
+        )}
+
+        
     </>)
     
 }
