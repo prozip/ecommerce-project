9cfbb73e9aedc6ee501667e8458c702226cf8006
GaKon101 <tronglong28@gmail.com>
2022-08-18 18:12:10 +0700

    done login and logout

--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -66,7 +66,7 @@ const LoginScreen = () => {
 
         <Row className='py-3'>
             <Col>
-                New Customer? <Link to={redirect ? `/register?redirect=${redirect}` : '/register' }></Link>
+                New Customer? <Link to={redirect ? `/register?redirect=${redirect}` : '/register' }>Register</Link>
             </Col>
         </Row>
 
