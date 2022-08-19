474ab4910e167d54d3f2f4cf8bdb444d469b145a
prozip <quachkimson2000@gmail.com>
2022-08-19 16:45:05 +0700

    fix navigate

--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -1,10 +1,10 @@
-import React, {useState, useEffect} from 'react'
-import {Link, useLocation, useNavigate} from 'react-router-dom'
-import { Form, Button, Row, Col} from 'react-bootstrap'
-import {useDispatch, useSelector} from 'react-redux'
+import React, { useState, useEffect } from 'react'
+import { Link, useLocation, useNavigate } from 'react-router-dom'
+import { Form, Button, Row, Col } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
 import Message from '../components/Message'
 import FormContainer from '../components/FormContainer'
-import Loader  from '../components/Loader'
+import Loader from '../components/Loader'
 import { login } from '../actions/userActions'
 
 const LoginScreen = () => {
@@ -17,9 +17,10 @@ const LoginScreen = () => {
     const dispatch = useDispatch()
 
     const userLogin = useSelector(state => state.userLogin)
-    const { loading, error, userInfo} = userLogin
+    const { loading, error, userInfo } = userLogin
 
-    const redirect = location.search ? location.search.split('=')[1] : '/'
+    // fix navigate to root
+    const redirect = location.search ? '/' + location.search.split('=')[1] : '/'
 
     useEffect(() => {
         if (userInfo) {
@@ -33,45 +34,45 @@ const LoginScreen = () => {
     }
 
 
-  return (
-    <FormContainer>
-        <h1>Sign In</h1>
-        {error && <Message variant='danger'>{error}</Message>}
-        {loading && <Loader />}
-        <Form onSubmit={submitHandler}>
-            <Form.Group controlId='email'>
-                 <Form.Label>Email Address</Form.Label>
-                 <Form.Control 
-                    type='email' 
-                    placeholder='Enter Email' 
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
+    return (
+        <FormContainer>
+            <h1>Sign In</h1>
+            {error && <Message variant='danger'>{error}</Message>}
+            {loading && <Loader />}
+            <Form onSubmit={submitHandler}>
+                <Form.Group controlId='email'>
+                    <Form.Label>Email Address</Form.Label>
+                    <Form.Control
+                        type='email'
+                        placeholder='Enter Email'
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
                     ></Form.Control>
-            </Form.Group>
+                </Form.Group>
 
-            <Form.Group controlId='password'>
-                 <Form.Label>Password Address</Form.Label>
-                 <Form.Control 
-                    type='password' 
-                    placeholder='Enter Password' 
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
+                <Form.Group controlId='password'>
+                    <Form.Label>Password Address</Form.Label>
+                    <Form.Control
+                        type='password'
+                        placeholder='Enter Password'
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
                     ></Form.Control>
-            </Form.Group>
+                </Form.Group>
 
-            <Button type='submit' variant='primary'>
-                Sign In
-            </Button>
-        </Form>
+                <Button type='submit' variant='primary'>
+                    Sign In
+                </Button>
+            </Form>
 
-        <Row className='py-3'>
-            <Col>
-                New Customer? <Link to={redirect ? `/register?redirect=${redirect}` : '/register' }>Register</Link>
-            </Col>
-        </Row>
+            <Row className='py-3'>
+                <Col>
+                    New Customer? <Link to={redirect ? `/register?redirect=${redirect}` : '/register'}>Register</Link>
+                </Col>
+            </Row>
 
-    </FormContainer>
-  )
+        </FormContainer>
+    )
 }
 
 export default LoginScreen
