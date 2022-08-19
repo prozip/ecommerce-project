5a913a4cb6931dfab873f2b02826f1626d3994cf
GaKon101 <tronglong28@gmail.com>
2022-08-19 18:02:29 +0700

    done Check Out Steps

--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -67,7 +67,7 @@ const LoginScreen = () => {
 
             <Row className='py-3'>
                 <Col>
-                    New Customer? <Link to={redirect ? `/register?redirect=${redirect}` : '/register'}>Register</Link>
+                    New Customer?{' '} <Link to={redirect ? `/register?redirect=${redirect}` : '/register'}>Register</Link>
                 </Col>
             </Row>
 
