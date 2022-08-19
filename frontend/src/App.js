65a885198d38aba9169bbd6bfb3ebdcb1d43cf2f
GaKon101 <tronglong28@gmail.com>
2022-08-19 12:02:43 +0700

    done Update User Profile

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,7 @@ import ProductScreen from './screens/ProductScreen'
 import CartScreen from './screens/CartScreen'
 import LoginScreen from './screens/LoginScreen'
 import RegisterScreen from './screens/RegisterScreen'
-
+import ProfileScreen from './screens/ProfileScreen'
 
 
 const App = () => {
@@ -21,6 +21,7 @@ const App = () => {
           <Routes>
             <Route path='/login' element={<LoginScreen />} />
             <Route path='/register' element={<RegisterScreen />} />
+            <Route path='/profile' element={<ProfileScreen />} />
             <Route path='/product/:id' element={<ProductScreen />} />
             <Route path='/cart/:id' element={<CartScreen />} />
             <Route path='/' element={<HomeScreen/>} exact />
