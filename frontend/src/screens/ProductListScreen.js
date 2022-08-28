236a2827664cfba1c705f0beeac6069bf6def7a8
GaKon101 <tronglong28@gmail.com>
2022-08-28 11:20:02 +0700

    Edit Product Screen

--- frontend/src/screens/ProductListScreen.js
+++ frontend/src/screens/ProductListScreen.js
@@ -5,7 +5,12 @@ import { Table, Button, Row , Col} from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
-import { listProducts, deleteProduct } from '../actions/productActions'
+import { 
+    listProducts, 
+    deleteProduct,
+    createProduct, 
+} from '../actions/productActions'
+import { PRODUCT_CREATE_RESET } from '../constants/productConstants'
 
 
 const ProductListScreen = () => {
@@ -24,17 +29,39 @@ const ProductListScreen = () => {
         success: successDelete,
     } = productDelete
 
+    const productCreate = useSelector(state => state.productCreate)
+    const { 
+        loading: loadingCreate, 
+        error: errorCreate, 
+        success: successCreate,
+        product: createdProduct,
+    } = productCreate
+
 
     const userLogin = useSelector(state => state.userLogin)
     const { userInfo } = userLogin
 
     useEffect(() => {
-        if(userInfo && userInfo.isAdmin){
+        dispatch({ type: PRODUCT_CREATE_RESET })
+        if(!userInfo.isAdmin){
+            navigate('/login') 
+        }
+
+        if(successCreate){
+            navigate(`/admin/product/${createdProduct._id}/edit`)
+        }else {
             dispatch(listProducts())
-        }else{
-            navigate('/login')
-        } 
-    }, [dispatch, navigate, userInfo, successDelete])
+        }
+
+
+    }, [
+        dispatch, 
+        navigate, 
+        userInfo, 
+        successDelete, 
+        successCreate, 
+        createdProduct,
+    ])
 
     const deleteHandler = (id) => {
         if(window.confirm('Are you sure?')){
@@ -43,10 +70,10 @@ const ProductListScreen = () => {
     }
 
     const createProductHandler = (product) => {
-        //create product
+        dispatch(createProduct())
     }
 
-    return (
+    return ( 
         <>
             <Row className='align-items-center'>
                 <Col>
@@ -61,6 +88,8 @@ const ProductListScreen = () => {
             </Row>
             {loadingDelete && <Loader />}
             {errorDelete && <Message variant='danger'>{errorDelete}</Message>}
+            {loadingCreate && <Loader />}
+            {errorCreate && <Message variant='danger'>{errorCreate}</Message>}
    
             {loading ? <Loader /> : error ? <Message variant='danger'>{error}</Message> : (
                 <Table striped bordered hover responsive className='table-sm'>
