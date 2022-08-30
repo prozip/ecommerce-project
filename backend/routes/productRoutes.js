13fdbb8f2a75c16239986ce93d5c241c9080d286
GaKon101 <tronglong28@gmail.com>
2022-08-30 22:12:06 +0700

    Morgan Create Reviews Endpoint

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -5,6 +5,7 @@ import {
     deleteProduct,
     createProduct,
     updateProduct,
+    createProductReview,
 } from "../controllers/productController.js";
 import { protect, admin } from "../middleware/authMiddleware.js";
 
@@ -14,6 +15,10 @@ router
     .route('/')
     .get(getProduct)
     .post(protect, admin, createProduct)
+
+    router
+    .route('/:id/reviews')
+    .post(protect, createProductReview) 
     
 router
     .route('/:id')
